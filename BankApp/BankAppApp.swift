670757451c16b2e8c8ab4df30e08f486dd9eab8e
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-27 20:17:10 +0530

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Sajed Shaikh on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
